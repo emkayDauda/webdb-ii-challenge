2c2023025f245eb5870be543f47a8e9e81158e75
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-05 14:14:25 +0100

    :sparkles: Write up and down migrations

--- data/migrations/20191105140719_build-cars-table.js
+++ data/migrations/20191105140719_build-cars-table.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', table => {
+      table.increments('id');
+
+      table.text('VIN').notNullable();
+
+      table.text('make').notNullable();
+
+      table.text('model').notNullable();
+
+      table.decimal('mileage').notNullable();
+
+      table.text('transmission', 6);
+
+      table.text('status');
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
